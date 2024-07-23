c0777c1fe1d1d1d2a2129483185e8d5363080f2f
Anderson de Carvalho Fernandes <andersoncarvalhofer@gmail.com>
2024-07-23 16:47:45 -0300

    First commit

---
+++ Conexao.php
@@ -0,0 +1,12 @@
+<?php
+$servidor = 'localhost';
+$usuario  = 'anddec10_ei';
+$senha 	  = 'Mel540879@';
+$banco    = 'anddec10_ei';
+
+// FUNCOES DO SISTEMA DE CADASTRO ###########
+
+$nomesite  = 'NOMEDASUAEMPRESA';
+$urlmaster = 'https://cheguei.delivery'; // APENAS A URL PRINCIPAL SEM A BARRA NO FINAL ---- ----
+
+date_default_timezone_set('America/Sao_Paulo');
---
+++ clientedemo/db/base.php
@@ -0,0 +1,3 @@
+<?php
+define('HOME', 'http://cheguei.delivery/clientedemo/');
+setlocale( LC_ALL, 'pt_BR', 'pt_BR.iso-8859-1', 'pt_BR.utf-8', 'portuguese' );
